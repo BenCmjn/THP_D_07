26408d3a830a73adc36d4e5915c3b1f8abd321c6
BenCmjn <benjamin.calmejane@gmail.com>
2018-01-16 20:06:43 +0100

    start of word passed succesfully

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -2,22 +2,49 @@
 def echo(sentence)
 	return sentence
 end
-
 echo("hello")
-
+#---------------------------------------------
 def shout(blabla)
 	return blabla.upcase
 end
-
 shout("hello")
-
+#---------------------------------------------
 def repeat(sentence,number_of_times=2)
-	# string = number_of_times.times {|sentence| sentence }
-
 	final = sentence
 	number_of_times -= 1
-
 	number_of_times.times {final += " #{sentence}"}
-
 	return final
 end
+#---------------------------------------------
+def start_of_word(word, n)
+	n -= 1
+	# n = 0 # J'pensais que ça allait 
+	word[0..n]
+
+	start_of_word("hello", 1)
+end
+#--------- Tous les bouts de codes péchés sur Google
+# qui servent à pas grand chose, en fait : ---------
+
+	# word.split("")
+	#word.each_char { |a| n += a }
+	
+	# for n in 0..word.length
+
+	# for position in 0..n
+	# 	range += pos 
+	# end
+
+	# range = word.select { |a| a < n }
+	
+	# return range
+
+	# range = word.length
+	#word [n]
+####-------------------------
+	# a.each_char { |c|
+	#   puts c
+	# }
+
+	# for pos in 0...s.length
+	# arr.select { |a| a > 3 }
