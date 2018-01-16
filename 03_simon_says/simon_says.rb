df1d4365fd86defac247c4c59070324bde2ab043
BenCmjn <benjamin.calmejane@gmail.com>
2018-01-16 18:33:56 +0100

    Simon says jusqu'à repeat passé

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -4,3 +4,20 @@ def echo(sentence)
 end
 
 echo("hello")
+
+def shout(blabla)
+	return blabla.upcase
+end
+
+shout("hello")
+
+def repeat(sentence,number_of_times=2)
+	# string = number_of_times.times {|sentence| sentence }
+
+	final = sentence
+	number_of_times -= 1
+
+	number_of_times.times {final += " #{sentence}"}
+
+	return final
+end
