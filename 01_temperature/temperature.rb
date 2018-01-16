f77335e78a2c2188016871e9bb60ad1e5133d894
BenCmjn <benjamin.calmejane@gmail.com>
2018-01-16 12:42:53 +0100

    Le test de la température est passé

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc(temperature)
+	temperature = (temperature-32)/1.8
+	temperature = temperature.round
+end
+
+def ctof(temperature)
+	temperature = temperature*1.8+32
+	temperature = temperature.round(1)
+end
